c36ee5d3ad26a3cfbd0a77d8cbce86746f997ee8
turtleStew <38766126+turtleStew@users.noreply.github.com>
2019-09-26 11:56:05 -0400

    progress

--- inc/Devices.h
+++ inc/Devices.h
@@ -22,21 +22,36 @@
  * 	-is responsible for "managing" the other devices and contains a device interrupt queue which lists the device IDs which sent an interrupt signal. Functions like a north bridge/south bridge
  */
 
-//Bit layout for dio: 00 000000 00000000 - select and interrupt signals, instruction, and data. However, devices can use this hwoever they are written to
+//Bit layout for dio: 00 000000 00000000 - select and interrupt signals, instruction, and data. However, devices can use this however they are written to
 
 //https://stackoverflow.com/questions/10110905/simple-sound-wave-generator-with-sdl-in-c
+//https://stackoverflow.com/questions/20753726/rendering-pixels-from-array-of-rgb-values-in-sdl-1-2
 
 SDL_Window *win;
 SDL_Renderer *ren;
 SDL_Event ev;
 
-unsigned int resW = 800, resH = 600, resA = resW * resH;
+const unsigned int resW = 400, resH = 240;
+
+unsigned int select = 0; //Selects which device has been called. ID assignment is hard coded for now
 
 //Device data
 typedef struct {
-	unsigned char vram[4 * resA];
-	unsigned char vrom[512];
-} gpuData;
+	//The vram is divided into 2 render buffers and a font page. There are 95 ASCII characters to support
+	unsigned char vram[(4 * (400 * 240) * 2) + (95 * 64 * 4)]; //color format * total resolution area in pixels * number of buffer pages + font page size. bufferAccess = (buffer * (y * resW + x) + rgbaSelect)
+	unsigned char vrom[1024];
+	unsigned char regs[16]; //Required regs: pc, acc, tmp, buff, cbuf, page, id, idp
+	SDL_Surface *buffers[2];
+	//Registers:
+	//	-pc
+	//	-acc
+	//	-tmp
+	//	-buff: selects the current render buffer
+	//	-cbuf: stores the render color
+	//	-page: ???
+	//	-id: stores the currently selected id byte pair
+	//	-idp: stores the rom mem pointer to the id info
+} gpu;
 
 //Device initialization funtions
 void init_gpu() {
@@ -46,14 +61,21 @@ void init_gpu() {
 
 //Device update funtions
 void update_gpu() {
-	//Process instructions
-	//Output graphics
-	for(unsigned int y = 0; y < resH; y++) {
-		for(unsigned int x = 0; x < resW; x++) {
-			//get rgba from vram, y * width + x
-			//draw pixel
-		}
+	if(select == 0) {
+		//Process instructions
+		//Instructions are received as a 16 bit register
+		//	-mov   arg1, arg2		- moves register 1 to register 2
+		//	-getb0 vram[arg1], arg2 - moves an address in the portion of vram used by buffer 0 to arg2
+		//	-getb1 vram[arg1], arg2 - moves an address in the portion of vram used by buffer 1 to arg2
+		//	-getf  vram[arg1], arg2 - moves an address in the portion of vram used by the font page to arg2
+		//	-setb0 arg1, vram[arg2] - moves arg1 to an address in the portion of vram used by buffer 0
+		//	-setb1 arg1, vram[arg2] - moves arg1 to an address in the portion of vram used by buffer 1
+		//	-setf  arg1, vram[arg2]	- moves arg1 to an address in the portion of vram used by the font page
+		//	-stm   arg1				- sets the gpu to data stream mode. The first bit of the register specifies if the stream is to receive data or send it back to the cpu, while the remaining bits select the register to stream
+		//	-swp					- toggles between the two render buffers
+		//	-asc   arg1				- renders the character at vram address arg1
 	}
+	//render output
 }
 
 //DIO functions
