03a72374ebb444e1a5ccf6751f0bf536b35c37ef
turtleStew <38766126+turtleStew@users.noreply.github.com>
2019-12-05 17:10:46 -0500

    fixed a few things

--- main.c
+++ main.c
@@ -5,37 +5,36 @@
 #include <stdio.h>
 #include <pthread.h>
 
+
+int execute = 1;
+int mem = 0;
+
+//keep the memory sizes as multiples of 2 else the emulator will crash if pc + 1 > memSize. The last two bytes of each memory array contains a hard coded jump instruction to pc = 0
+unsigned int ramSize = 2048;
+unsigned int romSize = 1024;
+unsigned int regCount = 16;
+unsigned int dioCount = 16;
+
+unsigned char ram[2048];
+unsigned char rom[1024];
+unsigned short regs[16];
+unsigned short dio[16];
+
+pthread_t cpuThread;//, deviceThread;
+
+void *cpuUpdateWrapper() {
+	execInstr((unsigned short*)regs, (unsigned short*)dio, (unsigned char*)ram, (unsigned char*)rom, ramSize, romSize, &mem, &execute);
+	return NULL;
+}
+
 int main(int argc, char *argv[]) {
-	//Vars - keep the memory sizes as multiples of 2 else the emulator will crash if pc + 1 > memSize. The last two bytes of each memory array contains a hard coded jump instruction to pc = 0
-	unsigned int ramSize = 2048;
-	unsigned int romSize = 1024;
-	unsigned int regCount = 16;
-	unsigned int dioCount = 16;
-
-	unsigned char ram[ramSize];
-	unsigned char rom[romSize];
-	unsigned short regs[regCount];
-	unsigned short dio[dioCount];
-
-	int execute = 1;
-	int mem = 0;
-
-	pthread_t cpuThread;//, deviceThread;
-
 	//Initialize
 	initDevices((unsigned short*)dio);
 
-	for(unsigned int i = 0; i < romSize; i++) { //These are temporary
-		rom[i] = 0;
-	}
-
-	for(unsigned int i = 0; i < ramSize; i++) {
-		ram[i] = 0;
-	}
-
-	for(unsigned int i = 0; i < regCount; i++) {
-		regs[i] = 0;
-	}
+	//These are temporary?
+	for(unsigned int i = 0; i < romSize; i++) rom[i] = 0;
+	for(unsigned int i = 0; i < ramSize; i++) ram[i] = 0;
+	for(unsigned int i = 0; i < regCount; i++) regs[i] = 0;
 
 	//Load rom
 	FILE *data = fopen("rom.bin", "rb");
@@ -45,15 +44,18 @@ int main(int argc, char *argv[]) {
 	fread(rom, dataSize, 1, data);
 	fclose(data);
 
-	pthread_create(cpuThread, NULL, &execInstr, (unsigned short*)regs, (unsigned short*)dio, (unsigned char*)ram, (unsigned char*)rom, ramSize, romSize, &mem, &execute);
+	//pthread_create(&cpuThread, NULL, &cpuUpdateWrapper, NULL);
+	//char buffer[8192];
 
+	//setvbuf(stdout, buffer, _IOFBF, sizeof(buffer));
 	//Loop
 	while(execute) {
-		//execInstr((unsigned short*)regs, (unsigned short*)dio, (unsigned char*)ram, (unsigned char*)rom, ramSize, romSize, &mem, &execute);
+		execInstr((unsigned short*)regs, (unsigned short*)dio, (unsigned char*)ram, (unsigned char*)rom, ramSize, romSize, &mem, &execute);
 		updateDevices((unsigned short*)dio, &regs[3], &execute);
+		//pthread_join(cpuThread, NULL);
 	}
 	
-	pthread_exit(NULL);
+	//pthread_exit(NULL);
 	end();
 
 	return 0;
